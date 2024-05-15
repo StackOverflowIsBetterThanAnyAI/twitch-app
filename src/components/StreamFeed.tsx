79abd0f070ffbbf8e6b0786c2fd1f1c1af953132
Michael Münzenhofer <michael.muenzenhofer@stud.th-deg.de>
2024-05-15 19:22:34 +0200

    TODO list

--- src/components/StreamFeed.tsx
+++ src/components/StreamFeed.tsx
@@ -1,14 +1,14 @@
-import { getStreams } from '../helper/getStreams'
-
-import { CLIENT_ID, CLIENT_SECRET } from '../clientdata/clientdata'
 import { useEffect, useState } from 'react'
 
+import { CLIENT_ID, CLIENT_SECRET } from '../clientdata/clientdata'
+
+import { getStreams } from '../helper/getStreams'
+
 import { StreamProps } from '../types/StreamProps'
-import { getImage } from '../helper/getImage'
 
 import { useScreenWidth } from '../hooks/useScreenWidth'
+
 import StreamLive from './StreamLive'
-import { getProfilePicture } from '../helper/getProfilePicture'
 import StreamThumbnail from './StreamThumbnail'
 import StreamProfilePicture from './StreamProfilePicture'
 import StreamViewerCount from './StreamViewerCount'
@@ -16,6 +16,31 @@ import StreamTags from './StreamTags'
 import StreamGame from './StreamGame'
 import StreamTitle from './StreamTitle'
 import StreamChannel from './StreamChannel'
+
+// TODO: fallback images for thumbnail, and more?
+
+// TODO: adjust thumbnail size
+
+// TODO: check if api calls are on the right place in the code
+
+// TODO: check if api error handling works
+
+// TODO: api result can also be empty or wrong
+
+// TODO: parameters and imports are sorted alphabetically
+
+// TODO: remove unused import statements and variables
+
+// TODO: check if something is re-rendered / executed too often
+// and can be replaced by a state variable
+
+// TODO: implement loading screen for initial page loading
+
+// TODO: add function which refreshes the page every eg three minutes
+
+// TODO: implement function which lets the user filter the results
+
+// TODO: check if more than twenty streams can be loaded by this or another api
 
 const StreamFeed = () => {
     const [streamData, setStreamData] = useState<StreamProps | undefined>(
@@ -37,11 +62,31 @@ const StreamFeed = () => {
         fetchData()
     }, [])
 
+    // TODO: should be turned into CSS.Properties
+    // => x < 700px looks meh, there should actually be two
+    // cards next to each other instead of only one
+    const gridClassName = (() => {
+        switch (screenWidth) {
+            case 'MOBILE':
+                return 'grid grid-cols-1'
+            case 'TABLET':
+                return 'grid grid-cols-2 gap-4'
+            case 'DESKTOP':
+                return 'grid grid-cols-3 gap-4'
+        }
+    })()
+
     return (
-        <div>
+        // TODO: turn style into className if possible
+        <div
+            style={{
+                gridTemplateColumns: 'repeat(auto-fit, minmax(320px, 1fr))',
+            }}
+            className="p-4 gap-4 grid"
+        >
             {streamData?.data &&
                 streamData.data.map((item) => (
-                    <article key={item.user_id} className="pb-4">
+                    <article key={item.user_id}>
                         <div className="relative pb-2">
                             <StreamThumbnail
                                 thumbnail_url={item.thumbnail_url}
@@ -57,8 +102,8 @@ const StreamFeed = () => {
                                 viewer_count={item.viewer_count}
                             />
                         </div>
-                        <div className="grid grid-cols-5 grid-rows-1 w-96">
-                            <div className="p-1 col-span-1">
+                        <div className="grid grid-cols-5 grid-rows-1 w-full">
+                            <div className="p-1 col-span-1 mx-auto">
                                 <StreamProfilePicture
                                     screenWidth={screenWidth}
                                     user_id={item.user_id}
@@ -69,7 +114,7 @@ const StreamFeed = () => {
                                 <StreamChannel user_name={item.user_name} />
                                 <StreamTitle title={item.title} />
                                 <StreamGame game_name={item.game_name} />
-                                <div className="flex flex-wrap w-64">
+                                <div className="flex flex-wrap w-full">
                                     {item.tags.map((item) => (
                                         <StreamTags item={item} key={item} />
                                     ))}
