a3eba3c80c1adcf2bd2eab5c5f5836a3fe11eff7
Michael Münzenhofer <michael.muenzenhofer@stud.th-deg.de>
2024-06-23 23:18:07 +0200

    remove empty strings in getSearchFilter function, apply getSearchFIlter to handleChange

--- src/components/UI/navigation/Navigation.tsx
+++ src/components/UI/navigation/Navigation.tsx
@@ -64,40 +64,21 @@ const Navigation = () => {
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchText(e.target.value)
-        const searchTextLowerCase = e.target.value
-            .toLowerCase()
-            .replaceAll(' ', '')
         if (streamData) {
-            const filteredData = streamData.data.filter(
-                (item) =>
-                    item.user_name
-                        .toLowerCase()
-                        .replaceAll(' ', '')
-                        .includes(searchTextLowerCase) ||
-                    item.game_name
-                        .toLowerCase()
-                        .replaceAll(' ', '')
-                        .includes(searchTextLowerCase) ||
-                    item.title
-                        .toLowerCase()
-                        .replaceAll(' ', '')
-                        .includes(searchTextLowerCase) ||
-                    item.tags.some((tag) =>
+            const results = getSearchFilter(e.target.value, streamData)!.map(
+                (item) => ({
+                    game_name: item.game_name,
+                    user_name: item.user_name,
+                    title: item.title,
+                    tags: item.tags.filter((tag) =>
                         tag
                             .toLowerCase()
-                            .replaceAll(' ', '')
-                            .includes(searchTextLowerCase)
-                    )
+                            .includes(
+                                e.target.value.toLowerCase().replaceAll(' ', '')
+                            )
+                    ),
+                })
             )
-
-            const results = filteredData.map((item) => ({
-                game_name: item.game_name,
-                user_name: item.user_name,
-                title: item.title,
-                tags: item.tags.filter((tag) =>
-                    tag.toLowerCase().includes(searchTextLowerCase)
-                ),
-            }))
             setSearchResults(results)
         }
         if (e.target.value.length === 0) {
--- src/helper/getSearchFilter.tsx
+++ src/helper/getSearchFilter.tsx
@@ -4,15 +4,27 @@ export const getSearchFilter = (
     searchText: string,
     streamData: StreamProps | undefined
 ) => {
-    const searchTextLowerCase = searchText.toLowerCase()
+    const searchTextLowerCase = searchText.toLowerCase().replaceAll(' ', '')
     if (streamData) {
         return streamData.data.filter(
             (item) =>
-                item.user_name.toLowerCase().includes(searchTextLowerCase) ||
-                item.game_name.toLowerCase().includes(searchTextLowerCase) ||
-                item.title.toLowerCase().includes(searchTextLowerCase) ||
+                item.user_name
+                    .toLowerCase()
+                    .replaceAll(' ', '')
+                    .includes(searchTextLowerCase) ||
+                item.game_name
+                    .toLowerCase()
+                    .replaceAll(' ', '')
+                    .includes(searchTextLowerCase) ||
+                item.title
+                    .toLowerCase()
+                    .replaceAll(' ', '')
+                    .includes(searchTextLowerCase) ||
                 item.tags.some((tag) =>
-                    tag.toLowerCase().includes(searchTextLowerCase)
+                    tag
+                        .toLowerCase()
+                        .replaceAll(' ', '')
+                        .includes(searchTextLowerCase)
                 )
         )
     }
