1ad971df02102191c9d0164bf9ef0886b88805f9
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2024-07-30 21:19:54 +0200

    fix e2e

--- cypress/e2e/tiwtch_app_is_live_large_screen.cy.js
+++ cypress/e2e/tiwtch_app_is_live_large_screen.cy.js
@@ -27,7 +27,7 @@ describe('twitch-app is live for large screen sizes', () => {
 
         cy.get('[data-testid="navigation-homepage-anchor"]')
             .invoke('attr', 'href')
-            .should('eq', '/')
+            .should('eq', '/twitch-app')
 
         cy.get('[data-testid="navigation-homepage-anchor"]')
             .children()
