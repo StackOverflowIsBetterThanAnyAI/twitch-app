0b70897240abb49a59e5e53525697af96768a549
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2024-08-08 15:06:46 +0200

    new footer e2e test

---
+++ cypress/e2e/footer.cy.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress" />
+
+describe('twitch-app contains footer', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('displays the footer', () => {
+        cy.get('[data-testid="footer-container"]').should('have.length', 1)
+
+        cy.get('[data-testid="footer-container"]')
+            .children()
+            .should('have.length', 1)
+    })
+
+    it('contains the correct footer content', () => {
+        cy.get('[data-testid="footer-content"]').should('have.length', 1)
+
+        cy.get('[data-testid="footer-content"]').contains(
+            'Copyright © 2024 Michael Münzenhofer. All Rights Reserved.GitHub Repository.'
+        )
+
+        cy.get('[data-testid="footer-content"]')
+            .children()
+            .should('have.length', 1)
+    })
+
+    it('links to the correct GitHub repository', () => {
+        cy.get('[data-testid="footer-anchor"]').should('have.length', 1)
+
+        cy.get('[data-testid="footer-anchor"]')
+            .invoke('attr', 'href')
+            .should(
+                'eq',
+                'https://github.com/StackOverflowIsBetterThanAnyAI/twitch-app'
+            )
+    })
+})
