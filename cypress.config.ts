71f2f8b1df96855058b9ec6745df0ac06df5337b
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-05-07 00:01:12 +0200

    update cypress url

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
     e2e: {
-        baseUrl: 'https://muenzenhofer.com',
+        baseUrl: 'https://stackoverflowisbetterthananyai.github.io/twitch-app',
     },
 })
